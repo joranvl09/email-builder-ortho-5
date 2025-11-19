5729c6ea6aac7c450d95e4b88f73f40583679257
joranvl09 <joranvleeuwen@gmail.com>
2025-11-19 16:57:11 +0100

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -4,6 +4,7 @@ class EmailBuilder {
         this.templates = [];
         this.currentEmail = [];
         this.editingBlockIndex = null;
+        this.draggedBlockIndex = null;
         
         this.initializeDefaultData();
         this.bindEvents();
@@ -14,116 +15,61 @@ class EmailBuilder {
         const defaultBlocks = [
             { 
                 id: 1, 
-                text: "Aanhef:\nBeste meneer/mevrouw,\n\n", 
+                text: "Aanhef:\nBeste [Klantnaam],\n\n", 
                 category: "aanhef" 
             },
             { 
                 id: 2, 
-                text: "Afsluiting:\nMet vriendelijke groet,\nHet Team Ortho Care", 
+                text: "Afsluiting:\nMet vriendelijke groet,\n\nHet team van Orto", 
                 category: "afsluiting" 
             },
             { 
                 id: 3, 
-                text: "Contact:\nTelefoon: 010-1234567\nE-mail: info@orthocare.nl", 
+                text: "Contact:\nTelefoon: [Telefoonnummer]\nE-mail: [E-mailadres]", 
                 category: "contact" 
             },
             { 
                 id: 4, 
-                text: "Bevestiging:\nHartelijk dank voor uw aanvraag. Wij hebben alle informatie goed ontvangen.", 
-                category: "dank" 
-            },
-            { 
-                id: 5, 
-                text: "Afspraak:\nWe nemen binnen twee werkdagen contact met u op voor een afspraak.", 
+                text: "Afspraak bevestiging:\nUw afspraak is bevestigd op [Datum] om [Tijd].", 
                 category: "afspraak" 
             },
             { 
+                id: 5, 
+                text: "Locatie:\nOnze locatie: [Adres]\nGelieve 10 minuten eerder aanwezig te zijn.", 
+                category: "locatie" 
+            },
+            { 
                 id: 6, 
-                text: "Kosten:\nTijdens het gesprek lichten we de kosten en planning uitgebreid toe.", 
-                category: "kosten" 
+                text: "Annulering:\nMocht u verhinderd zijn, gelieve dit 24 uur van tevoren door te geven.", 
+                category: "annulering" 
             },
             { 
                 id: 7, 
-                text: "Vragen:\nHeeft u nog vragen vooraf? Laat het ons vooral weten.", 
-                category: "vragen" 
+                text: "Feedback vraag:\nHoe ervaart u uw herstel tot nu toe?", 
+                category: "feedback" 
             },
             { 
                 id: 8, 
-                text: "Samenwerking:\nWe kijken uit naar onze samenwerking en hopen u snel te mogen helpen.", 
-                category: "samenwerking" 
-            },
-            { 
-                id: 9, 
-                text: "Onderwerp:\nBetreft: Offerte aanvraag orthopedische zorg", 
-                category: "onderwerp" 
-            },
-            { 
-                id: 10, 
-                text: "Verwijzing:\nNaar aanleiding van uw verwijzing van huisarts Dr. Jansen...", 
-                category: "verwijzing" 
+                text: "Nazorg:\nHet is belangrijk om nazorgafspraken of controles bij te wonen.", 
+                category: "nazorg" 
             }
         ];
 
         const defaultTemplates = [
             { 
                 id: 1, 
-                name: "Standaard Reactie",
+                name: "Afspraak Bevestiging",
                 content: [
-                    "Beste meneer/mevrouw,\n\nHartelijk dank voor uw bericht. Wij hebben uw vraag ontvangen en nemen binnen 24 uur contact met u op.\n\nMet vriendelijke groet,\nHet Team Ortho Care\n\nTelefoon: 010-1234567\nE-mail: info@orthocare.nl"
+                    "Beste [Klantnaam],\n\nWij willen u graag informeren dat uw afspraak bij Orto is bevestigd op [Datum] om [Tijd]. De afspraak vindt plaats op onze locatie aan [Adres]. Gelieve bij aankomst ongeveer 10 minuten eerder aanwezig te zijn, zodat wij u rustig kunnen inchecken en de administratie kunnen afronden.\n\nWat u kunt verwachten tijdens uw bezoek:\n\n• Een korte intake door een van onze specialisten\n• Eventuele metingen of onderzoeken die nodig zijn voor uw behandeling\n• Bespreking van uw behandelplan en eventuele vervolgafspraken\n\nIndien u speciale wensen of vragen heeft over uw behandeling, laat het ons gerust weten voor uw afspraak. U kunt ons bereiken via [Telefoonnummer] of [E-mailadres].\n\nMocht u verhinderd zijn, vragen wij u vriendelijk dit uiterlijk 24 uur voor de afspraak aan ons door te geven, zodat wij een nieuwe afspraak voor u kunnen plannen. Dit helpt ons om onze planning efficiënt te houden en andere klanten ook optimaal te kunnen bedienen.\n\nWij kijken ernaar uit u te verwelkomen en u te helpen met uw behandeling.",
+                    "Met vriendelijke groet,\n\nHet team van Orto"
                 ]
             },
             { 
                 id: 2, 
-                name: "Offerte Aanvraag",
+                name: "Nazorg en Feedback",
                 content: [
-                    "Beste meneer/mevrouw,\n\nBedankt voor uw interesse in onze orthopedische diensten. We hebben uw aanvraag ontvangen en gaan hier graag verder op in.",
-                    "Tijdens een persoonlijk gesprek bespreken we uw specifieke situatie, wensen en mogelijkheden. We lichten dan ook de kosten en planning uitgebreid toe.",
-                    "We nemen binnen twee werkdagen contact met u op om een afspraak in te plannen. Heeft u vooraf nog vragen? Laat het ons weten.\n\nMet vriendelijke groet,\nHet Team Ortho Care"
-                ]
-            },
-            { 
-                id: 3, 
-                name: "Afspraak Bevestiging",
-                content: [
-                    "Beste meneer/mevrouw,\n\nHierbij bevestigen wij uw afspraak op [datum] om [tijd] uur.\nLocatie: Ortho Care Centrum, Hoofdstraat 123, Rotterdam",
-                    "Tijdens de afspraak bespreken we uw situatie en maken we een behandelplan op maat. Graag vragen wij u eventuele medische gegevens en verwijsbrief mee te nemen.",
-                    "Mocht u verhinderd zijn, laat het ons dan minimaal 24 uur van tevoren weten zodat we de afspraak kunnen verplaatsen.\n\nWe kijken uit naar uw komst!\n\nMet vriendelijke groet,\nHet Team Ortho Care"
-                ]
-            },
-            { 
-                id: 4, 
-                name: "Factuur Herinnering",
-                content: [
-                    "Beste meneer/mevrouw,\n\nHierbij een vriendelijke herinnering voor de openstaande factuur met nummer [factuurnummer] met een totaalbedrag van € [bedrag].",
-                    "Zou u de betaling zo spoedig mogelijk willen voldoen? Indien u de factuur reeds heeft betaald, kunt u deze herinnering als niet verzonden beschouwen.",
-                    "Voor vragen over deze factuur kunt u contact opnemen met onze administratie via telefoonnummer 010-1234567 of per e-mail: admin@orthocare.nl\n\nBij voorbaat dank voor uw medewerking."
-                ]
-            },
-            { 
-                id: 5, 
-                name: "Klachtenafhandeling",
-                content: [
-                    "Geachte heer/mevrouw,\n\nHartelijk dank voor het melden van uw klacht. Onze excuses voor het ongemak dat u heeft ervaren.",
-                    "We nemen uw feedback zeer serieus en doen ons uiterste best om een passende oplossing te vinden voor uw situatie.",
-                    "U hoort binnen 48 uur van onze klantenservice coördinator. Mocht u in de tussentijd vragen hebben, dan kunt u terecht bij [naam contactpersoon].\n\nMet vriendelijke groet,\nHet Management Ortho Care"
-                ]
-            },
-            { 
-                id: 6, 
-                name: "Samenwerking Voorstel",
-                content: [
-                    "Beste [Naam],\n\nBedankt voor het waardevolle gesprek van [datum]. We zijn enthousiast over de mogelijke samenwerking.",
-                    "Bijgevoegd vindt u ons uitgebreide voorstel met daarin onze aanpak, planning en investering. We zijn ervan overtuigd dat we u optimaal kunnen ondersteunen.",
-                    "We kijken uit naar uw reactie en hopen snel van u te horen. Voor vragen staat ons team uiteraard graag voor u klaar.\n\nMet vriendelijke groet,\n[Je Naam]\nOrtho Care Specialist"
-                ]
-            },
-            { 
-                id: 7, 
-                name: "Vacature Reactie",
-                content: [
-                    "Beste meneer/mevrouw,\n\nHartelijk dank voor uw sollicitatie op de functie van Orthopedisch Medewerker. We hebben uw CV en motivatie met interesse ontvangen.",
-                    "Uw profiel sluit goed aan bij onze zoektocht. We nodigen u graag uit voor een kennismakingsgesprek in week [weeknummer].",
-                    "U hoort binnen 5 werkdagen van ons voor het inplannen van een gesprek. Heeft u vragen vooraf? Neem dan contact op met onze HR afdeling.\n\nMet vriendelijke groet,\nHet Werving & Selectie Team"
+                    "Beste [Klantnaam],\n\nWij hopen dat uw behandeling bij Orto goed is verlopen en dat u tevreden bent met het resultaat. Ons doel is om ervoor te zorgen dat u zich volledig ondersteund voelt, niet alleen tijdens de behandeling, maar ook in de periode erna.\n\nOm u optimaal te begeleiden, willen wij graag uw feedback ontvangen over het volgende:\n\n• Hoe ervaart u uw herstel tot nu toe?\n• Heeft u nog vragen over de oefeningen, instructies of materialen die u heeft gekregen?\n• Zijn er problemen of ongemakken die wij voor u kunnen oplossen?\n\nDaarnaast willen wij u eraan herinneren dat het belangrijk is om eventuele nazorgafspraken of controles bij te wonen. Dit helpt ons om uw herstel goed te monitoren en bij te sturen waar nodig.\n\nIndien u aanvullende ondersteuning nodig heeft, kunt u contact opnemen via [Telefoonnummer] of [E-mailadres]. Wij staan altijd klaar om uw vragen te beantwoorden en u verder te helpen.\n\nWij waarderen uw vertrouwen in Orto en kijken ernaar uit om u te blijven ondersteunen bij uw herstel en welzijn.",
+                    "Met vriendelijke groet,\n\nHet team van Orto"
                 ]
             }
         ];
@@ -169,6 +115,7 @@ class EmailBuilder {
     setupDragAndDrop() {
         const emailCanvas = document.getElementById('emailCanvas');
         
+        // Drop zone voor nieuwe blokken
         emailCanvas.addEventListener('dragover', (e) => {
             e.preventDefault();
             emailCanvas.classList.add('drag-over');
@@ -183,8 +130,42 @@ class EmailBuilder {
             emailCanvas.classList.remove('drag-over');
             
             const blockId = e.dataTransfer.getData('text/plain');
-            this.addBlockToEmail(parseInt(blockId));
+            if (blockId.startsWith('block-')) {
+                // Bestaand blok verslepen
+                const fromIndex = parseInt(blockId.split('-')[1]);
+                const toIndex = this.getDropIndex(e);
+                this.moveBlock(fromIndex, toIndex);
+            } else {
+                // Nieuw blok toevoegen
+                this.addBlockToEmail(parseInt(blockId));
+            }
         });
+    }
+
+    getDropIndex(e) {
+        const emailCanvas = document.getElementById('emailCanvas');
+        const blocks = emailCanvas.querySelectorAll('.email-block');
+        const canvasRect = emailCanvas.getBoundingClientRect();
+        const y = e.clientY - canvasRect.top;
+
+        for (let i = 0; i < blocks.length; i++) {
+            const blockRect = blocks[i].getBoundingClientRect();
+            const blockTop = blockRect.top - canvasRect.top;
+            const blockMiddle = blockTop + (blockRect.height / 2);
+
+            if (y < blockMiddle) {
+                return i;
+            }
+        }
+        return blocks.length;
+    }
+
+    moveBlock(fromIndex, toIndex) {
+        if (fromIndex === toIndex) return;
+        
+        const block = this.currentEmail.splice(fromIndex, 1)[0];
+        this.currentEmail.splice(toIndex, 0, block);
+        this.renderEmail();
     }
 
     renderBlocks() {
@@ -199,7 +180,7 @@ class EmailBuilder {
             blockElement.dataset.id = block.id;
 
             blockElement.addEventListener('dragstart', (e) => {
-                e.dataTransfer.setData('text/plain', block.id);
+                e.dataTransfer.setData('text/plain', block.id.toString());
                 blockElement.classList.add('dragging');
             });
 
@@ -250,7 +231,18 @@ class EmailBuilder {
         this.currentEmail.forEach((block, index) => {
             const blockElement = document.createElement('div');
             blockElement.className = 'email-block';
+            blockElement.draggable = true;
             blockElement.textContent = block.text;
+            blockElement.dataset.index = index;
+
+            blockElement.addEventListener('dragstart', (e) => {
+                e.dataTransfer.setData('text/plain', `block-${index}`);
+                blockElement.classList.add('dragging');
+            });
+
+            blockElement.addEventListener('dragend', () => {
+                blockElement.classList.remove('dragging');
+            });
 
             const editBtn = document.createElement('button');
             editBtn.textContent = '✏️ Bewerk';
